c4f20624a2182fb534a4c4c9a161c74e1bfa72f5
BarteKKmita <bartekkmitaa@gmail.com>
2019-12-30 13:00:49 +0100

    "Added "twin" Gym class visible for Client in controller."

---
+++ Gym-Star/src/main/java/com/learning/gym/star/gym/controller/GymFrameForController.java
@@ -0,0 +1,26 @@
+package com.learning.gym.star.gym.controller;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Getter
+public class GymFrameForController{
+
+    @ApiModelProperty(hidden = true)
+    private String gymId = null;
+    @NotEmpty
+    private String gymName;
+    @NotEmpty
+    private String street;
+    @NotEmpty
+    private String city;
+    @NotEmpty
+    private String buildingNumber;
+}
+
