edb87bf9d9e13a4fc950871cf06f374e67c931d9
BarteKKmita <bartekkmitaa@gmail.com>
2019-12-30 21:18:32 +0100

    "Added global class for exception handling. Every exception not cached with @ExceptionHandler in Controller class will be handled here."

---
+++ Gym-Star/src/main/java/com/learning/gym/star/gym/service/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.learning.gym.star.gym.service;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler{
+
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity handleContentNotAllowedException(){
+        return new ResponseEntity <>("FileNotFound", HttpStatus.CONFLICT);
+    }
+
+}
