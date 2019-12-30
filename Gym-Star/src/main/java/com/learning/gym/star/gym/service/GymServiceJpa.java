9b48c5a29cc10794d575acfa135aeadbdb6cafd1
BarteKKmita <bartekkmitaa@gmail.com>
2019-12-30 19:40:42 +0100

    "Added GymService interface to make possible dependency injection in GymController class. By qualifier it is possible to choose right service and autowired it. "

---
+++ Gym-Star/src/main/java/com/learning/gym/star/gym/service/GymServiceJpa.java
@@ -0,0 +1,47 @@
+package com.learning.gym.star.gym.service;
+
+import com.learning.gym.star.gym.Gym;
+import com.learning.gym.star.gym.controller.GymFrameForController;
+import com.learning.gym.star.gym.database.GymJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("GymServiceJpa")
+public class GymServiceJpa implements GymService{
+    private GymJpaRepository gymRepository;
+    private GymSerializer gymSerializer;
+
+    @Autowired
+    public GymServiceJpa(GymJpaRepository gymRepository, GymSerializer gymSerializer){
+        this.gymRepository = gymRepository;
+        this.gymSerializer = gymSerializer;
+    }
+
+    @Override
+    public List <String> getAllGyms(){
+        return null;
+    }
+
+    @Override
+    public String addGym(GymFrameForController gym){
+        return gymRepository.save(gymSerializer.getGymFromGymFrame(gym)).getGymId();
+    }
+
+    @Override
+    public void updateGym(Gym gym, int gymId){
+
+    }
+
+    @Override
+    public GymFrameForController getGymById(int gymId){
+        Gym databaseGym = gymRepository.findById(Integer.toString(gymId)).orElse(null);
+        return gymSerializer.getGymFrameFromGym(databaseGym);
+    }
+
+    @Override
+    public void deleteGymById(int gymId){
+
+    }
+}
