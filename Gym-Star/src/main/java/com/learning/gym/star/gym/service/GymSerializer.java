ea4afb339515a97dc2995e56c06ba060f1521571
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-03 20:12:37 +0100

    Add Gym builder in Serializer class

--- Gym-Star/src/main/java/com/learning/gym/star/gym/service/GymSerializer.java
+++ Gym-Star/src/main/java/com/learning/gym/star/gym/service/GymSerializer.java
@@ -6,6 +6,9 @@ import com.learning.gym.star.gym.Gym;
 import com.learning.gym.star.gym.controller.GymFrameForController;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class GymSerializer{
     private static ObjectMapper objectMapper = new ObjectMapper();
@@ -14,14 +17,15 @@ public class GymSerializer{
         String gymAsJson = "";
         try {
             gymAsJson = objectMapper.writeValueAsString(gymFrame);
-
         } catch(JsonProcessingException e) {
+            System.out.println("Serialization of gym failure.");
             e.printStackTrace();
         }
         Gym databaseGym = null;
         try {
             databaseGym = objectMapper.readValue(gymAsJson, Gym.class);
         } catch(JsonProcessingException e) {
+            System.out.println("Deserialization of gym failure.");
             e.printStackTrace();
         }
         return databaseGym;
@@ -42,5 +46,21 @@ public class GymSerializer{
             e.printStackTrace();
         }
         return gymFrame;
+    }
+
+    public GymFrameForController buildGymFrameForController(String[] gymAsStringArray){
+        return GymFrameForController.builder()
+                .gymId(gymAsStringArray[0])
+                .gymName(gymAsStringArray[1])
+                .street(gymAsStringArray[2])
+                .city(gymAsStringArray[3])
+                .buildingNumber(gymAsStringArray[4])
+                .build();
+    }
+
+    public List <GymFrameForController> buildGymListForController(List <Gym> gymsFromDatabase){
+        List <GymFrameForController> gymListForController = new ArrayList <>();
+        gymsFromDatabase.forEach(gym -> gymListForController.add(buildGymFrameForController(gym.toStringArray())));
+        return gymListForController;
     }
 }
