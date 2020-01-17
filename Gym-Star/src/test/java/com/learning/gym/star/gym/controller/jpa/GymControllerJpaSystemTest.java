d3c7058f1a1c3b8b6f1a725f6fe88dbc7b4beb7b
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-17 13:01:36 +0100

    Add system tests for Jpa controller

---
+++ Gym-Star/src/test/java/com/learning/gym/star/gym/controller/jpa/GymControllerJpaSystemTest.java
@@ -0,0 +1,44 @@
+package com.learning.gym.star.gym.controller.jpa;
+
+import com.learning.gym.star.gym.database.jpa.GymJpaRepository;
+import com.learning.gym.star.gym.service.jpa.GymServiceJpa;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GymControllerJpaSystemTest {
+
+    @Autowired
+    GymControllerJpa controllerJpa;
+    GymJpaRepository repository;
+    GymServiceJpa serviceJpa;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldControllerNotBeNullWhenAutowired(){
+        assertNotNull(controllerJpa);
+    }
+
+    @Test
+    public void shouldGetSecondGymFromRealMySQLDatabaseWhenGetGymWithId2() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jpa/gym/{id}", "2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gymName").value("druga"));
+    }
+
+}
