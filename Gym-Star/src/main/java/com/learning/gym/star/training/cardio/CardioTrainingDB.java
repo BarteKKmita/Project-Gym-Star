acb27ebdc63f9bde4769bdc80482d25f21bfc132
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-18 19:09:23 +0100

    Move cardio classes to cardio package

---
+++ Gym-Star/src/main/java/com/learning/gym/star/training/cardio/CardioTrainingDB.java
@@ -0,0 +1,27 @@
+package com.learning.gym.star.training.cardio;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Entity
+@Table(name = "cardiotraining")
+public class CardioTrainingDB {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cardio_id")
+    @NotNull
+    private String cardio_Id;
+
+    @Column(name = "training_count ")
+    @NotEmpty
+    private Integer trainingCount;
+}
---
+++ Gym-Star/src/main/java/com/learning/gym/star/training/cardio/service/CardioTrainingService.java
@@ -0,0 +1,31 @@
+package com.learning.gym.star.training.cardio.service;
+
+import com.learning.gym.star.training.cardio.CardioTrainingDB;
+import com.learning.gym.star.training.cardio.database.CardioTrainingJpaRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("CardioTrainingService")
+@NoArgsConstructor
+public class CardioTrainingService {
+    @Autowired
+    CardioTrainingJpaRepository repository;
+
+    public CardioTrainingService(CardioTrainingJpaRepository repository){
+        this.repository = repository;
+    }
+
+    public int getCardioTrainingCount(int cardioId){
+        CardioTrainingDB training = repository.findById(String.valueOf(cardioId)).orElseThrow();
+        return training.getTrainingCount();
+    }
+
+    public void doCardioTraining(int cardioId){
+        repository.updateTrainingCounter(String.valueOf(cardioId));
+    }
+
+    public void resetCardioStatistics(int cardioId){
+        repository.resetCardioStatistics(String.valueOf(cardioId));
+    }
+}
