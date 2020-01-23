221e7577335cf4d2d03657954b6b090e60b9ba52
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-23 17:38:06 +0100

    Add statistics service

--- Gym-Star/src/main/java/com/learning/gym/star/statistics/statisticsdb/service/StatisticsService.java
+++ Gym-Star/src/main/java/com/learning/gym/star/statistics/statisticsdb/service/StatisticsService.java
@@ -4,33 +4,32 @@ import com.learning.gym.star.statistics.statisticsdb.StatisticsDB;
 import com.learning.gym.star.statistics.statisticsdb.database.StatisticsRepository;
 import com.learning.gym.star.training.cardio.CardioTrainingDB;
 import com.learning.gym.star.training.power.PowerTrainingDB;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
+import java.util.List;
 
 @Service("statistics service")
 public class StatisticsService {
 
     private StatisticsRepository repository;
-    @Autowired
-    private EntityManager entityManager;
 
     public StatisticsService(StatisticsRepository repository){
         this.repository = repository;
     }
 
     public int createNewStatistics(){
-
         StatisticsDB stats = StatisticsDB.builder()
                 .cardioTrainingDB(new CardioTrainingDB())
                 .powerTrainingDB(new PowerTrainingDB())
                 .build();
-        return Integer.parseInt(repository.save(stats).getStatisticsId());
+        return Integer.parseInt(repository.saveAndFlush(stats).getStatisticsId());
     }
 
-    public StatisticsDB readAllStatistics(int statisticsId){
+    public List<StatisticsDB> readAllStatistics(){
+        return repository.findAll();
+    }
+
+    public StatisticsDB readStatisticsById(int statisticsId){
         return repository.findById(String.valueOf(statisticsId)).orElseThrow();
     }
-
 }
