c9a72adbafca8c65eb3bc2d3cb79876b48083ee1
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-23 17:39:36 +0100

    Add statistics controller

--- Gym-Star/src/main/java/com/learning/gym/star/statistics/statisticsdb/controller/StatisticsController.java
+++ Gym-Star/src/main/java/com/learning/gym/star/statistics/statisticsdb/controller/StatisticsController.java
@@ -1,9 +1,9 @@
 package com.learning.gym.star.statistics.statisticsdb.controller;
 
 import com.learning.gym.star.statistics.statisticsdb.service.StatisticsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/statistics")
@@ -13,6 +13,16 @@ public class StatisticsController {
 
     public StatisticsController(StatisticsService service){
         this.service = service;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity getAllStatistics(){
+        return new ResponseEntity<>(service.readAllStatistics(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getStatisticsById(@PathVariable("id") int statisticsId){
+        return new ResponseEntity<>(service.readStatisticsById(statisticsId), HttpStatus.OK);
     }
 
     @PostMapping("/create")
