a7fc0f1f409737a72265d381dc1306fc36cddc62
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-27 19:58:41 +0100

    Refactor Rest Date and Time

--- Gym-Star/src/main/java/com/learning/gym/star/statistics/timedb/TrainingDateStatisticsDB.java
+++ Gym-Star/src/main/java/com/learning/gym/star/statistics/timedb/TrainingDateStatisticsDB.java
@@ -1,8 +1,11 @@
-package com.learning.gym.star.statistics.time;
+package com.learning.gym.star.statistics.timedb;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.learning.gym.star.statistics.statisticsdb.StatisticsDB;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -11,11 +14,11 @@ import java.time.LocalTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Getter
 @Entity
 @Table(name = "sportsmentrainingtimestatistics")
 public class TrainingDateStatisticsDB {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "statistics_id")
@@ -31,7 +34,7 @@ public class TrainingDateStatisticsDB {
 
     @JsonIgnore
     @ManyToOne()
-    @JoinColumn(
-            name = "sportsmanstats_id", referencedColumnName = "statistics_id")
+    @JoinColumn(name = "sportsmanstats_id",
+            referencedColumnName = "statistics_id")
     private StatisticsDB sportsmanStatsId;
 }
