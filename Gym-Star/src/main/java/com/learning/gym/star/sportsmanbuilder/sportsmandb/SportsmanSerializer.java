ebc58fd80b3c6b77dc9992b0a345ea713664a4a2
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-28 18:42:37 +0100

    Add logger to sportsman serializer

--- Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/SportsmanSerializer.java
+++ Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/SportsmanSerializer.java
@@ -2,14 +2,13 @@ package com.learning.gym.star.sportsmanbuilder.sportsmandb;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-/*
-This class will use Gson as soon as I will handle with dependency conflict.
-Logger will be added after pull request.
- */
 @Configuration
 public class SportsmanSerializer {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     public SportsmanDTO getSportsmanDTOFromSportsman(SportsmanDB sportsman){
@@ -17,31 +16,33 @@ public class SportsmanSerializer {
         try {
             sportsmanJson = objectMapper.writeValueAsString(sportsman);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Serialization of Sportsman failure.", e);
+            logger.debug("Sportsman data. {}", sportsman);
         }
-
         SportsmanDTO sportsmanDTO = null;
         try {
             sportsmanDTO = objectMapper.readValue(sportsmanJson, SportsmanDTO.class);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Deserialization of Sportsman failure.", e);
+            logger.debug("Sportsman data. {}", sportsman);
         }
         return sportsmanDTO;
     }
 
     public SportsmanDB getSportsmanDBFromSportsmanDTO(SportsmanDTO sportsman){
-        String sportsmanJson = "";
+        String sportsmanDTOJson = "";
         try {
-            sportsmanJson = objectMapper.writeValueAsString(sportsman);
+            sportsmanDTOJson = objectMapper.writeValueAsString(sportsman);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Serialization of SportsmanDTO failure.", e);
+            logger.debug("SportsmanDTO data. {}", sportsman);
         }
-
         SportsmanDB sportsmanDB = null;
         try {
-            sportsmanDB = objectMapper.readValue(sportsmanJson, SportsmanDB.class);
+            sportsmanDB = objectMapper.readValue(sportsmanDTOJson, SportsmanDB.class);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Deserialization of SportsmanDTO failure.", e);
+            logger.debug("SportsmanDTO data. {}", sportsman);
         }
         return sportsmanDB;
     }
