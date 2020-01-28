3ee7cdbc48ba7e5572cf35d0476852a6cada309e
BarteKKmita <bartekkmitaa@gmail.com>
2020-01-28 18:04:16 +0100

    Add logger to power training

--- Gym-Star/src/main/java/com/learning/gym/star/training/power/controller/PowerTrainingController.java
+++ Gym-Star/src/main/java/com/learning/gym/star/training/power/controller/PowerTrainingController.java
@@ -1,6 +1,8 @@
 package com.learning.gym.star.training.power.controller;
 
 import com.learning.gym.star.training.power.service.PowerTrainingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +12,7 @@ import java.util.NoSuchElementException;
 @RestController()
 @RequestMapping("api/power")
 public class PowerTrainingController {
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private PowerTrainingService service;
 
     public PowerTrainingController(PowerTrainingService service){
@@ -19,23 +21,27 @@ public class PowerTrainingController {
 
     @GetMapping("{id}")
     public ResponseEntity getPowerTrainingCount(@PathVariable("id") int powerId){
+        logger.info("Attempting to get power training count");
         return new ResponseEntity<>(service.getPowerTrainingCount(powerId), HttpStatus.OK);
     }
 
     @PutMapping("/train/{id}")
     @ResponseStatus(HttpStatus.OK)
     void doPowerTraining(@PathVariable("id") int powerId){
+        logger.info("Attempting to increment power training count");
         service.doPowerTraining(powerId);
     }
 
     @PutMapping("/reset/{id}")
     @ResponseStatus(HttpStatus.OK)
     void resetPowerStatistics(@PathVariable("id") int powerId){
+        logger.info("Attempting to reset power training count");
         service.resetPowerStatistics(powerId);
     }
 
     @PostMapping("/create")
     public ResponseEntity createNewStatistics(){
+        logger.info("Attempting to create new power training");
         return new ResponseEntity<>("Your gym id nr : " + service.createNewPowerStatistics(), HttpStatus.CREATED);
     }
 
