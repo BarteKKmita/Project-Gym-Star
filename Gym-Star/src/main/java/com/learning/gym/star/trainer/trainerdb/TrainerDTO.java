f7ea5405ea0bdfc94b8f64f0e6b34010715b5f0d
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-02 17:56:32 +0100

    Add pesel validation to TrainerDTO

--- Gym-Star/src/main/java/com/learning/gym/star/trainer/trainerdb/TrainerDTO.java
+++ Gym-Star/src/main/java/com/learning/gym/star/trainer/trainerdb/TrainerDTO.java
@@ -2,9 +2,11 @@ package com.learning.gym.star.trainer.trainerdb;
 
 
 import lombok.*;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /*
@@ -18,12 +20,15 @@ Validation will be added in the future.
 @Getter
 public class TrainerDTO implements Serializable {
 
+    @PESEL(message = "Pesel must have 11 digits")
     @NotNull(message = "Please enter pesel number.")
-    private Long pesel;
+    private CharSequence pesel;
 
+    @Size(min = 2, message = "Name is to short")
     @NotEmpty(message = "Trainer has to have name")
     private String name;
 
+    @Size(min = 2, message = "Surname is to short")
     @NotEmpty(message = "Trainer has to have surname")
     private String surname;
 
