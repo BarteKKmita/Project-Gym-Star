dc2387665ddb31550674056cb0e57c805a0faf0e
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-02 18:09:57 +0100

    Add pesel validation to SportsmanDTO

--- Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/SportsmanDTO.java
+++ Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/SportsmanDTO.java
@@ -2,9 +2,11 @@ package com.learning.gym.star.sportsmanbuilder.sportsmandb;
 
 import com.learning.gym.star.sportsmanbuilder.gender.GenderChoose;
 import lombok.*;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Builder
 @NoArgsConstructor
@@ -12,14 +14,19 @@ import javax.validation.constraints.NotNull;
 @ToString
 @Getter
 public class SportsmanDTO {
-    @NotNull(message = "Please enter pesel number.")
-    private Long sportsmanPesel;
 
+    @PESEL(message = "Pesel must have 11 digits")
+    @NotNull(message = "Please enter pesel number.")
+    private CharSequence sportsmanPesel;
+
+    @Size(min = 2, message = "Name is to short")
     @NotEmpty(message = "Sportsman has to have name")
     private String name;
 
+    @Size(min = 2, message = "Surname is to short")
     @NotEmpty(message = "Sportsman has to have surname")
     private String surname;
+
 
     @NotNull(message = "Please specify sportsman gender")
     private GenderChoose gender;
