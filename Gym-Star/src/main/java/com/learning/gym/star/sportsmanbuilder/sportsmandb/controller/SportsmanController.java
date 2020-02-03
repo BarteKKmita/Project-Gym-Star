a6c2093a20df3b98c88e315e9a10f884d370dd93
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-03 20:38:31 +0100

    Add pesel validation to sportsman controller

--- Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/controller/SportsmanController.java
+++ Gym-Star/src/main/java/com/learning/gym/star/sportsmanbuilder/sportsmandb/controller/SportsmanController.java
@@ -2,20 +2,24 @@ package com.learning.gym.star.sportsmanbuilder.sportsmandb.controller;
 
 import com.learning.gym.star.sportsmanbuilder.sportsmandb.SportsmanDTO;
 import com.learning.gym.star.sportsmanbuilder.sportsmandb.service.SportsmanService;
+import org.hibernate.validator.constraints.pl.PESEL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityExistsException;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+@Validated
 @RestController
 @RequestMapping("/api/sportsman")
 public class SportsmanController {
@@ -35,34 +39,35 @@ public class SportsmanController {
 
     @GetMapping("/{pesel}/dateandtime")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity getSportsmanDateAndTimeStats(@PathVariable("pesel") CharSequence sportsmanPesel){
+    public ResponseEntity getSportsmanDateAndTimeStats(@PathVariable("pesel") @PESEL(message = "Pesel must have 11 digits") CharSequence sportsmanPesel){
         logger.info("Attempting to get sportsman date and time statistics.");
         return new ResponseEntity<>(service.getSportsmanStatistics(sportsmanPesel), HttpStatus.OK);
     }
 
     @PutMapping("/{pesel}/gettrainer")
     @ResponseStatus(HttpStatus.CREATED)
-    public void chooseTrainer(@PathVariable("pesel") CharSequence sportsmanPesel, @RequestBody CharSequence trainerPesel){
+    public void chooseTrainer(@PESEL(message = "Pesel must have 11 digits") @PathVariable("pesel") CharSequence sportsmanPesel,
+                              @PESEL(message = "Pesel must have 11 digits") @RequestBody CharSequence trainerPesel){
         logger.info("Attempting to choose trainer.");
         service.chooseTrainer(sportsmanPesel, trainerPesel);
     }
 
     @GetMapping("/mytrainer")
-    public ResponseEntity getMyTrainer(@RequestBody CharSequence sportsmanPesel){
+    public ResponseEntity getMyTrainer(@PESEL(message = "Pesel must have 11 digits") @RequestBody CharSequence sportsmanPesel){
         logger.info("Attempting to display sportsman's.");
         return new ResponseEntity<>(service.getMyTrainerData(sportsmanPesel), HttpStatus.OK);
     }
 
     @PutMapping("/trainCardio")
     @ResponseStatus(HttpStatus.OK)
-    public void trainCardio(@RequestBody CharSequence sportsmanPesel){
+    public void trainCardio(@PESEL(message = "Pesel must have 11 digits") @RequestBody CharSequence sportsmanPesel){
         logger.info("Attempting to do cardio training.");
         service.trainCardio(sportsmanPesel);
     }
 
     @PutMapping("/trainPower")
     @ResponseStatus(HttpStatus.OK)
-    public void trainPower(@RequestBody CharSequence sportsmanPesel){
+    public void trainPower(@PESEL(message = "Pesel must have 11 digits") @RequestBody CharSequence sportsmanPesel){
         logger.info("Attempting to do power training.");
         service.trainPower(sportsmanPesel);
     }
@@ -84,6 +89,15 @@ public class SportsmanController {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity handleValidationExceptions(ConstraintViolationException ex){
+        Map<Object, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(error -> {
+            errors.put(error.getInvalidValue(), error.getMessage());
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
