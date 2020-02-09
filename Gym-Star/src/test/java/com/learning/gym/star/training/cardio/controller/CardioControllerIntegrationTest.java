088b909cad3a9f7132ff969fdc9a5d92c21f1944
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-09 10:16:13 +0100

    Add cardio controller integration tests

---
+++ Gym-Star/src/test/java/com/learning/gym/star/training/cardio/controller/CardioControllerIntegrationTest.java
@@ -0,0 +1,92 @@
+package com.learning.gym.star.training.cardio.controller;
+
+import com.learning.gym.star.training.cardio.service.CardioTrainingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.NoSuchElementException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = CardioTrainingController.class)
+public class CardioControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CardioTrainingService service;
+
+    @Test
+    public void shouldListenToControllerEndpoint() throws Exception{
+        //Given
+        int cardioId = 1;
+        //Then
+        mockMvc.perform(get("/api/cardio/" + cardioId)
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotListenToOtherControllersEndpoint() throws Exception{
+        //Given
+        int powerId = 1;
+        //Then
+        mockMvc.perform(get("/api/power/" + powerId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturnStatusOKWhenUpdatingExistingCardioStats() throws Exception{
+        //Given
+        int cardioId = 1;
+        //Then
+        mockMvc.perform(put("/api/cardio/train/" + cardioId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnStatusNotFountWhenUpdatingNotExistingCardioStats() throws Exception{
+        //Given
+        int cardioId = 1;
+        doThrow(new NoSuchElementException()).when(service).doCardioTraining(any(Integer.class));
+        //Then
+        mockMvc.perform(put("/api/cardio/train/" + cardioId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturnStatusOKWhenResettingCardioStats() throws Exception{
+        //Given
+        int cardioId = 1;
+        //Then
+        mockMvc.perform(put("/api/cardio/reset/" + cardioId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnStatusOKWhenCreatingNewCardioStats() throws Exception{
+        //Then
+        mockMvc.perform(post("/api/cardio/create/"))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturnResponseBodyWhenCreatingNewCardioStats() throws Exception{
+        //Given
+        String cardioId = "1";
+        String responseBody = "Your gym id nr : " + cardioId;
+        when(service.createNewCardioStatistics()).thenReturn(cardioId);
+        //Then
+        mockMvc.perform(post("/api/cardio/create/")
+                .contentType("application/json")
+                .content(responseBody))
+                .andExpect(status().isCreated());
+    }
+}
