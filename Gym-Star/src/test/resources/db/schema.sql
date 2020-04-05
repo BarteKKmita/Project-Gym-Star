5df1683f7d8ddfe9aa2eb6be6e8acaed570262c5
BarteKKmita <bartekkmitaa@gmail.com>
2020-04-05 16:51:58 +0200

    Add statistics record for testing

--- Gym-Star/src/test/resources/db/schema.sql
+++ Gym-Star/src/test/resources/db/schema.sql
@@ -1,15 +1,15 @@
 
-CREATE TABLE PowerTraining (
+create TABLE PowerTraining (
 power_id INT PRIMARY KEY AUTO_INCREMENT,
 training_count INT DEFAULT 0
 );
 
-CREATE TABLE CardioTraining (
+create TABLE CardioTraining (
 cardio_id INT PRIMARY KEY AUTO_INCREMENT,
 training_count INT DEFAULT 0
 );
 
-CREATE TABLE Statistics (
+create TABLE Statistics (
 statistics_id INT PRIMARY KEY AUTO_INCREMENT,
 power_id INT,
 cardio_id INT,
@@ -17,7 +17,7 @@ FOREIGN KEY (power_id) REFERENCES PowerTraining(power_id),
 FOREIGN KEY (cardio_id) REFERENCES CardioTraining(cardio_id)
 );
 
-CREATE TABLE Gym (
+create TABLE Gym (
 gym_id INT PRIMARY KEY AUTO_INCREMENT,
 gym_name NVARCHAR(80) NOT NULL,
 street NVARCHAR (80) NOT NULL,
@@ -25,14 +25,14 @@ city NVARCHAR(30) NOT NULL,
 building_number INT NOT NULL
 );
 
-CREATE TABLE Trainer (
+create TABLE Trainer (
 trainer_pesel BIGINT PRIMARY KEY,
 trainer_name NVARCHAR(30) NOT NULL,
 trainer_surname NVARCHAR (80) NOT NULL,
 cost INT NOT NULL
 );
 
-CREATE TABLE SportsMen (
+create TABLE SportsMen (
 sportsman_pesel BIGINT PRIMARY KEY,
 sportsman_name NVARCHAR(30) NOT NULL,
 sportsman_surname NVARCHAR (80) NOT NULL,
@@ -43,19 +43,19 @@ FOREIGN KEY (trainer_pesel)  REFERENCES Trainer(trainer_pesel),
 FOREIGN KEY (statistics_id) REFERENCES Statistics(statistics_id)
 );
 
-ALTER TABLE SportsMen
-ADD COLUMN gym_id INT;
+alter table SportsMen
+add COLUMN gym_id INT;
 
-ALTER TABLE SportsMen
-ADD FOREIGN KEY (gym_id) REFERENCES Gym(gym_id);
+alter table SportsMen
+add FOREIGN KEY (gym_id) REFERENCES Gym(gym_id);
 
-ALTER TABLE Trainer
-ADD COLUMN gym_id INT;
+alter table Trainer
+add COLUMN gym_id INT;
 
-ALTER TABLE Trainer
-ADD FOREIGN KEY (gym_id) REFERENCES Gym(gym_id);
+alter table Trainer
+add FOREIGN KEY (gym_id) REFERENCES Gym(gym_id);
 
-CREATE TABLE SportsMenTrainingTimeStatistics (
+create TABLE SportsMenTrainingTimeStatistics (
 statistics_id INT PRIMARY KEY AUTO_INCREMENT,
 date DATE NOT NULL,
 time TIME NOT NULL,
@@ -63,24 +63,24 @@ sportsmanstats_id int NOT NULL,
 FOREIGN KEY (sportsmanstats_id) REFERENCES Statistics(statistics_id)
 );
 
-ALTER TABLE SportsMen
-ADD FOREIGN KEY (trainer_pesel) REFERENCES trainer(trainer_pesel);
+alter table SportsMen
+add FOREIGN KEY (trainer_pesel) REFERENCES trainer(trainer_pesel);
 
-ALTER TABLE Trainer
-ADD COLUMN sportsman BIGINT;
+alter table Trainer
+add COLUMN sportsman BIGINT;
 
-ALTER TABLE Trainer
-ADD FOREIGN KEY (sportsman) REFERENCES SportsMen(sportsman_pesel);
+alter table Trainer
+add FOREIGN KEY (sportsman) REFERENCES SportsMen(sportsman_pesel);
 
-CREATE TABLE trainersatgym (
+create TABLE trainersatgym (
 trainer_pesel BIGINT,
 gym_id INT,
 PRIMARY KEY(trainer_pesel, gym_id),
-FOREIGN KEY (trainer_pesel) REFERENCES trainer(trainer_pesel) ON DELETE CASCADE,
-FOREIGN KEY (gym_id) REFERENCES gym(gym_id) ON DELETE CASCADE);
+FOREIGN KEY (trainer_pesel) REFERENCES trainer(trainer_pesel) ON delete CASCADE,
+FOREIGN KEY (gym_id) REFERENCES gym(gym_id) ON delete CASCADE);
 
 DELIMITER //
-CREATE PROCEDURE getsportsmanstats (sportsmanStatsId INT)
+create procedure getsportsmanstats (sportsmanStatsId int)
 BEGIN
 SELECT statistics_id, date, time FROM SportsMenTrainingTimeStatistics
 WHERE sportsmanstats_id=sportsmanStatsId;
@@ -88,14 +88,17 @@ END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE trainCardio (statsId INT, cardiosportsmanID INT)
+create procedure trainCardio (statsId int, cardiosportsmanID int)
 BEGIN
 UPDATE cardioTraining SET training_count=training_count+1 WHERE cardio_id = cardiosportsmanID;
 INSERT INTO SportsMenTrainingTimeStatistics VALUES(NULL,curdate(),curtime(), statsId);
 END //
 DELIMITER ;
 
-INSERT INTO gym Values ('1','pierwsza', 'sezamkowa','rudy', 102);
-INSERT INTO gym Values ('2','druga', 'sezamkowa','kraków', 102);
-INSERT INTO cardiotraining values (1,0);
-INSERT INTO powertraining values (1,5);
+insert into gym values ('1','pierwsza', 'sezamkowa','rudy', 102);
+insert into gym values ('2','druga', 'sezamkowa','kraków', 102);
+insert into gym values ('4','Gym', 'ToDelete','kraków', 200);
+insert into cardiotraining values (1,0);
+insert into powertraining values (1,5);
+insert into Statistics values (1,1,1);
+insert into SportsMenTrainingTimeStatistics values (1,'2020-01-22','18:18:52',1);
