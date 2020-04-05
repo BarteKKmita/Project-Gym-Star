b218991c3a2652d5dcd38f9ca99df3220b7565d4
BarteKKmita <bartekkmitaa@gmail.com>
2020-04-05 17:43:06 +0200

    Add Controller Integration tests against Embedded MySQL

---
+++ Gym-Star/src/test/java/com/learning/gym/star/statistics/statisticsdb/controller/StatisticsControllerIntegrationEmbeddedMySQLTest.java
@@ -0,0 +1,83 @@
+package com.learning.gym.star.statistics.statisticsdb.controller;
+
+import com.learning.gym.star.EmbeddedMySqlProvider;
+import com.learning.gym.star.statistics.statisticsdb.database.StatisticsRepository;
+import com.learning.gym.star.statistics.statisticsdb.service.StatisticsService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class StatisticsControllerIntegrationEmbeddedMySQLTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private StatisticsController controller;
+
+    @Autowired
+    private StatisticsService service;
+
+    @Autowired
+    private StatisticsRepository repository;
+
+
+    @BeforeAll
+    public static void setUpClass(){
+        EmbeddedMySqlProvider.setUpClass();
+    }
+
+    @AfterAll
+    public static void tearDownClass(){
+        EmbeddedMySqlProvider.tearDownClass();
+    }
+
+    @Test
+    public void shouldFieldsNotBeNull(){
+        assertNotNull(mockMvc);
+        assertNotNull(controller);
+        assertNotNull(service);
+        assertNotNull(repository);
+    }
+
+    @Test
+    public void shouldAddStatsRecord() throws Exception{
+        //Given
+        var expectedStatsId = "2";
+        //When
+        mockMvc.perform(post("/api/statistics")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(expectedStatsId));
+    }
+
+    @Test
+    public void shouldGetStatsRecord() throws Exception{
+        //Given
+        var existingStatsId = "1";
+        //When
+        mockMvc.perform(get("/api/statistics/" + existingStatsId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.statisticsId").value(1))
+                .andExpect(jsonPath("$.cardioTrainingEntity.cardioId").value(1))
+                .andExpect(jsonPath("$.cardioTrainingEntity.trainingCount").value(0))
+                .andExpect(jsonPath("$.powerTrainingEntity.powerId").value(1))
+                .andExpect(jsonPath("$.powerTrainingEntity.trainingCount").value(5));
+    }
+}
